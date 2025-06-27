8040719c93efb029361bb524131e4880f6951319
Tilek <timur.almamatov@yandex.ru>
2025-06-27 16:56:44 +0700

    сделал базу верстки для создания статьи

--- src/components/newArticleForm/NewArticleForm.js
+++ src/components/newArticleForm/NewArticleForm.js
@@ -1,6 +1,22 @@
+import './NewArticleForm.scss';
+
 const NewArticleForm = () => {
     return (<form className="form-article">
-        asdasda
+        <div className="card-title">New Article</div>
+        <div className='form__wrap'>
+            <div><label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" placeholder='Title'/>
+            </div>
+            <div>
+                <label htmlFor="short-descr">Short description</label>
+                <input type="text" name="short-descr" id="short-descr" placeholder='short description'/>
+            </div>
+            <div>
+                <label htmlFor="text">Text</label>
+                <textarea  name="text" id="text" placeholder='text'/>
+            </div>
+        </div>
+        <input className="button button_blue" type='submit' value="Send"/>
     </form>);
 }
 
